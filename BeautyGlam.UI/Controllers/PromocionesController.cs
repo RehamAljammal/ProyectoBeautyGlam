dbb5de8298f2c60715917f4d8a53fb59fd3af34e
adriela <you@example.com>
2025-12-14 11:06:50 -0600

    modulo de promociones agregado

---
+++ BeautyGlam.UI/Controllers/PromocionesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyGlam.UI.Controllers
+{
+    public class PromocionesController : Controller
+    {
+        // GET: Promociones
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
