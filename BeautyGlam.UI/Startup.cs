4340c6cdd0e0175f53bc5a3892cb6ebfd5097a8b
Sharon8260 <sgomez70800@ufide.ac.cr>
2026-01-22 22:48:39 -0600

    Estructura

---
+++ BeautyGlam.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeautyGlam.UI.Startup))]
+namespace BeautyGlam.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
