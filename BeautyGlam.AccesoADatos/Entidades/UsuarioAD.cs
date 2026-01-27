16cde33c0e8cd8492e98920e31284b3edf3b60fe
Sharon8260 <sgomez70800@ufide.ac.cr>
2026-01-27 15:20:42 -0600

    Inventario

--- BeautyGlam.AccesoADatos/Entidades/UsuarioAD.cs
+++ BeautyGlam.AccesoADatos/Entidades/UsuarioAD.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BeautyGlam.AccesoADatos.Entidades
 {
+    [Table("Usuario")]
+
     public class UsuarioAD
     {
+        [Column("ID_USUARIO")]
+        public int idUsuario { get; set; }
+
+        [Column("NOMBRE")]
+        public string nombre { get; set; }
+
+        [Column("APELLIDO")]
+        public string apellido { get; set; }
+
+        [Column("USERNAME")]
+        public string username { get; set; }
+
+        [Column("CORREO")]
+        public string correo { get; set; }
+
+        [Column("TELEFONO")]
+        public string telefono { get; set; }
+
+        [Column("DIRECCION")]
+        public string direccion { get; set; }
+
+        [Column("FECHA_REGISTRO")]
+        public DateTime fechaRegistro { get; set; }
+
+        [Column("ASPNETUSERID")]
+        public string AspNetUserId { get; set; }
+
+        [Column("ESTADO")]
+        public bool estado { get; set; }
+
+
     }
 }
---
+++ BeautyGlam.AccesoADatos/Inventario/EditarStockActual/EditarStockActualAD.cs
@@ -0,0 +1,54 @@
+﻿using BeautyGlam.Abstracciones.AccesoADatos.Inventario.EditarStock;
+using BeautyGlam.Abstracciones.ModelosParaUI;
+using BeautyGlam.AccesoADatos.Entidades;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace BeautyGlam.AccesoADatos.Inventario.EditarInventario
+{
+    public class EditarStockActualAD : IEditarStockActualAD
+    {
+        private Contexto _elContexto;
+
+        public EditarStockActualAD()
+        {
+            _elContexto = new Contexto();
+        }
+
+        public async Task<int> Editar(InventarioDto elInventarioParaGuardar)
+        {
+            int filasAfectadas = 0;
+
+            InventarioAD inventarioEnBD =
+                await _elContexto.Inventario
+                .FirstOrDefaultAsync(i => i.idProducto == elInventarioParaGuardar.idProducto);
+
+            if (inventarioEnBD != null)
+            {
+                inventarioEnBD.stockActual = elInventarioParaGuardar.stockActual;
+                filasAfectadas = await _elContexto.SaveChangesAsync();
+            }
+
+            return filasAfectadas;
+        }
+
+        public async Task<InventarioDto> ObtenerPorProducto(int idProducto)
+        {
+            InventarioAD entidad =
+                await _elContexto.Inventario
+                .FirstOrDefaultAsync(i => i.idProducto == idProducto);
+
+            if (entidad == null)
+                return null;
+
+            return new InventarioDto
+            {
+                idInventario = entidad.idInventario,
+                stockActual = entidad.stockActual,
+                stockMinimo = entidad.stockMinimo,
+                stockMaximo = entidad.stockMaximo,
+                idProducto = entidad.idProducto
+            };
+        }
+    }
+}
