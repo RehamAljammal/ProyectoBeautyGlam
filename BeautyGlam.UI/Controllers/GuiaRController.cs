1e1748f153ba39c4373c1ecbc02dadb229197d7c
Sharon8260 <sgomez70800@ufide.ac.cr>
2026-02-02 19:01:15 -0600

    Guia

---
+++ BeautyGlam.UI/Controllers/GuiaRController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeautyGlam.UI.Controllers
+{
+    public class GuiaRController : Controller
+    {
+        // GET: GuiaR
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
